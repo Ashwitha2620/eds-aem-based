de12363c99549edbaa567cd2d3a46e88e9852630
Ashwitha S <130651553+Ashwitha2620@users.noreply.github.com>
2025-02-13 16:35:26 +0530

    Update imagetext.js

--- blocks/imagetext/imagetext.js
+++ blocks/imagetext/imagetext.js
@@ -1,7 +1,1 @@
-export default function decorate(block) {
-  const [imagetextWrapper] = block.children;
-  const blockquote = document.createElement('blockquote'); 
-  blockquote.textContent = imagetextWrapper.textContent.trim();  
-  imagetextWrapper.replaceChildren(blockquote);
-}
- 
+
